7f8006c27f15f99e47a5d09bd4a84e4d79244676
Wiseman325 <163990718+Wiseman325@users.noreply.github.com>
2024-09-09 18:46:34 +0000

    established foreignKey relationships on models

--- website/models.py
+++ website/models.py
@@ -1,9 +1,10 @@
 from . import create_app
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin;
 
 db = SQLAlchemy(create_app())
 
-class Student(db.Model):
+class Student(db.Model, UserMixin):
     _id = db.Column(db.Integer, primary_key=True)
     idNumber = db.Column(db.Integer)
     name = db.Column(db.String(80))
@@ -15,6 +16,8 @@ class Student(db.Model):
     physicalAddress = db.Column(db.String(80))
     highestGrade = db.Column(db.String(80))
     financialStatus = db.Column(db.String(80))
+    course_id = db.Column(db.Integer, db.ForeignKey('Course._id'), nullable=False)
+
 
 class Course(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
@@ -22,6 +25,7 @@ class Course(db.Model):
     description = db.Column(db.String(80))
     duration = db.Column(db.Integer(80))
     cost = db.Column(db.Integer(80))
+    student_id = db.relationship('Student')
 
 class Donor(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +35,8 @@ class Donor(db.Model):
     phoneNumber = db.Column(db.String(80))
     organisation = db.Column(db.String(80))
     amount = db.Column(db.Integer(80))
+    payment_id = db.relationship('Payment')
+    certificate_id = db.relationship('Certificate')
 
 
 class Certificate(db.Model):
@@ -39,13 +45,21 @@ class Certificate(db.Model):
     issuedDate = db.Column(db.Date)
     message = db.Column(db.String(80))
     signiture = db.Column(db.String(80))
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor._id'), nullable=False)
 
 
 class Payment(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Integer)
     date = db.Column(db.Date)
-    paymentMethod = db.Column(db.String(80))
+    paymentMethod = db.Column(db.String(100))
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor._id'), nullable=False)
 
+class FinancialAid(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student._id'), nullable=False)
+    donor_id = db.Column(db.Integer, db.ForeignKey('donor._id'), nullable=False)
+    application_status = db.Column(db.String(100))
+    application_date = db.Column(db.Date)
 
 
